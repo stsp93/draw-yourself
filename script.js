cf745d3795f48adce887396cdd6bd06492e3b88c
stsp93 <stspsp@gmail.com>
2023-07-25 21:00:47 +0300

    added talking

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const [rightForearm, leftForearm] = document.getElementsByClassName('forearm');
 const [rightArm, leftArm] = document.getElementsByClassName('arm');
 const [rightPupil, leftPupil] = document.getElementsByClassName('pupil')
+const button = document.querySelector('button');
+const mouthEl = document.getElementsByClassName('mouth')[0];
+const popupEl = document.querySelector('.popup');
 
 Array.from([leftForearm, leftArm]).forEach(el => {
     el.addEventListener('mouseover', function () {
@@ -15,6 +18,27 @@ Array.from([rightForearm, rightArm]).forEach(el => {
     })
 })
 
+let intervalId;
+
+button.addEventListener('click', function () {
+    mouthEl.classList.add('talk');
+
+    intervalId = setInterval(() => {
+        popupEl.textContent += `${generateRandomChars(4)}  `;
+    }, 1000)
+    popupEl.style.display = 'block';
+})
+
+function generateRandomChars(charCount) {
+    const characters = 'qertyuiopasdfghjklzxcvbnm!@#$%^&';
+    let output = '';
+    for (let i = 0; i < charCount; i++) {
+        randomChar = characters.charAt(Math.floor(Math.random() * characters.length))
+        output += randomChar
+    }
+    return output
+}
+
 // Animationend listeners
 
 leftArm.addEventListener('animationend', function () {
@@ -23,6 +47,13 @@ leftArm.addEventListener('animationend', function () {
 
 rightArm.addEventListener('animationend', function () {
     this.classList.remove('wave-right');
+})
+
+mouthEl.addEventListener('animationend', function () {
+    this.classList.remove('talk');
+    popupEl.style.display = 'none';
+    popupEl.textContent = '';
+    clearInterval(intervalId);
 })
 
 
